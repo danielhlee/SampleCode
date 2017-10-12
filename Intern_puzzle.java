ceccfde45873a086ff3cef73fcf5c38c573460dc
Daniel Lee <danielhlee44@gmail.com>
2017-10-11 23:51:34 -0700

    sample

---
+++ Intern_puzzle.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Intern_puzzle 
+{
+	public static void main(String[] args) throws Exception
+	{
+        // File input
+		File file = new File(args[0]);
+		Scanner in = new Scanner(file);
+		
+		// Number of people currently running
+		int currCount = 0; 
+		// Total number of people that need added so people will join
+		int countAdded= 0;
+		
+		while ( in.hasNextLine() )
+		{
+			// Minimum number of people that need to be running for people to join
+			int minNumber = in.nextInt();
+			// Number of people that will join each round
+			int joined = in.nextInt();
+			// Number of people added each row of input
+			int roundAdded = 0;
+			
+			if  (minNumber == 0)
+			{
+				currCount += joined;
+			}
+			else
+			{
+				if (currCount < minNumber)
+				{
+					roundAdded = minNumber - currCount;
+					countAdded += roundAdded; 
+					currCount += roundAdded + joined;
+				}
+				else
+				{
+					currCount += joined;
+				}
+			}
+		}
+		in.close();
+		System.out.println("Out: " + countAdded);
+	}
+}
