49c0628fc4cfdb8f5bdf17871c39e9c8f0c9d477
Daniel Lee <danielhlee44@gmail.com>
2017-10-17 10:35:50 -0700

    Update LargestValueInTreeRow.java

--- LargestValueInTree/LargestValueInTreeRow.java
+++ LargestValueInTree/LargestValueInTreeRow.java
@@ -10,7 +10,7 @@
 // }
 int[] largestValuesInTreeRows(Tree<Integer> t) 
 {
-    // null case
+    // Null tree case
     if (t == null)
     {
         int[] array = new int[0];
@@ -19,31 +19,39 @@ int[] largestValuesInTreeRows(Tree<Integer> t)
     else
     {
         Queue<Tree <Integer>> bfs = new LinkedList<Tree<Integer>>();
+        // ArrayList to keep track of largest values of each row
         ArrayList<Integer> list = new ArrayList<Integer>();
         bfs.add(t);
         int size = bfs.size();
         
         while (size > 0)
         {
+            // Set max to smallest integer
             int max = Integer.MIN_VALUE;
-            System.out.println(Integer.MIN_VALUE);
+            
+            // Loops through the tree for all integers in each row
             for (int i = 0; i < size; i++)
             {
                 Tree <Integer> current = bfs.poll();
                 max = Math.max(current.value, max);
+                // Add current nodes left child if it exists
                 if (current.left != null)
                 {
                     bfs.add(current.left);
                 }
+                 // Add current nodes right child if it exists
                 if (current.right != null)
                 {
                     bfs.add(current.right);
                 }
             }
+            // Add max integer from row to ArrayList
             list.add(max);
+            // Set size to number of integers in row
             size = bfs.size();
         }
             
+        // Convert ArrayList to array to return 
         int[] array = new int[list.size()];
         for (int j = 0; j < array.length; j++)
         {
