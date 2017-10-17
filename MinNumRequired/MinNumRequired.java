8ebc502912149d407cb50467b264e735d37b56ab
Daniel Lee <danielhlee44@gmail.com>
2017-10-17 09:53:55 -0700

    Update MinNumRequired.java

--- MinNumRequired/MinNumRequired.java
+++ MinNumRequired/MinNumRequired.java
@@ -3,45 +3,47 @@ import java.util.Scanner;
 
 public class minNumRequired
 {
-	public static void main(String[] args) throws Exception
+    public static void main(String[] args) throws Exception 
+    {
+        // File input
+        File file = new File(args[0]);
+        Scanner in = new Scanner(file);
+		
+	// Number of people currently running
+	int currCount = 0; 
+	// Total number of people that need added so people will join
+	int countAdded= 0;
+		
+	while ( in.hasNextLine() )
 	{
-                // File input
-		File file = new File(args[0]);
-		Scanner in = new Scanner(file);
-		
-		// Number of people currently running
-		int currCount = 0; 
-		// Total number of people that need added so people will join
-		int countAdded= 0;
-		
-		while ( in.hasNextLine() )
-		{
-			// Minimum number of people that need to be running for people to join
-			int minNumber = in.nextInt();
-			// Number of people that will join each row of input
-			int joined = in.nextInt();
-			// Number of people added each row of input
-			int roundAdded = 0;
+	    // Minimum number of people that need to be running for people to join
+	    int minNumber = in.nextInt();
+	    // Number of people that will join each row of input
+	    int joined = in.nextInt();
+	    // Number of people added each row of input
+	    int roundAdded = 0;
 			
-			if  (minNumber == 0)
-			{
-				currCount += joined;
-			}
-			else
-			{
-				if (currCount < minNumber)
-				{
-					roundAdded = minNumber - currCount;
-					countAdded += roundAdded; 
-					currCount += roundAdded + joined;
-				}
-				else
-				{
-					currCount += joined;
-				}
-			}
+	    if  (minNumber == 0)
+	    {
+	        currCount += joined;
+	    }
+	    else
+	    {
+	        if (currCount < minNumber)
+		{
+		    roundAdded = minNumber - currCount;
+		    countAdded += roundAdded; 
+		    currCount += roundAdded + joined;
 		}
-		in.close();
-		System.out.println("Out: " + countAdded);
+		else
+		{
+		    currCount += joined;
+		}
+	    } 
 	}
+	in.close();
+	
+	// Prints out the number of people added to race
+	System.out.println("Out: " + countAdded);
+    }
 }
